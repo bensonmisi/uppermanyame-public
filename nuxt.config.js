7acfacc73da0cd79b5b05fa6e0861b828dbf8058
Benson misi <misib@praz.org.zw>
2022-09-06 22:39:59 +0200

    changes 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000/api/v1'
+    baseURL: 'http://146.190.56.13/api/v1'
   },
   router: {
     middleware: ['auth']
